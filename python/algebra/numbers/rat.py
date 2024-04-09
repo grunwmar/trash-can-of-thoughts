2be26b1fd85dca81330e82a5a027278e74d6a506
Martin Grunwald <martingrunwald91@gmail.com>
2024-04-10 00:57:57 +0200

    #auto-commit: [Wed Apr 10 00:57:57 CEST 2024]

---
+++ python/algebra/numbers/rat.py
@@ -0,0 +1,10 @@
+from rational import Rational
+import sys
+
+f = float(sys.argv[1])
+
+r = Rational.from_float(f)
+
+print("\n", f, "=", r)
+print("\n", f, "=", r.strf("{quot} r {rem}"))
+print("\n", f, "=", r.strf("{quot} [{rem}/{div}]"), "\n")
