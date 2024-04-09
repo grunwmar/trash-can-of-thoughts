2c8a047537713b3ad861c0e997b2a22c34d6f17a
Martin Grünwald <mgrunwal@redhat.com>
2024-04-09 15:06:19 +0200

    #auto-commit: [Tue Apr  9 03:06:19 PM CEST 2024]

--- python/algebra/numbers/rational.py
+++ python/algebra/numbers/rational.py
@@ -27,11 +27,15 @@ class Rational(object):
         r = a % b
         return q, self.__class__(r, b)
 
-    def with_remainder(self):
+    def remainder(self, rational=True):
         a, b = self._val
         q = a // b
         r = a % b
-        return q, r
+
+        if rational:
+            return q, self.__class__(r, b)
+        else:
+            return q, r
 
 r = Rational(150, 26)
 
